e29a5db3df064b61d87a0ecd7bf2369fad0aa4bf
Max Simmons <maxsimmons20@gmail.com>
2023-05-13 17:13:19 -0500

    detail functionality is done

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,10 +1,29 @@
 import { useHistory } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
-
-
 function DetailsPage() {
     const history = useHistory();
+
+    const currentMovie = useSelector(state => state.detailsReducer);
+    const genreTable = useSelector(state => state.genres)
+    const movieID = currentMovie.id
+
+    console.log('current movie', currentMovie);
+    console.log('current ID', movieID);
+
+let movieDetails = genreTable.filter(function (genre) {
+    return genre.movie_id === movieID
+});
+
+let genres = movieDetails.map(function (detail) {
+    return detail.genre
+});
+
+console.log('movie details',movieDetails);
+
+console.log('genres', genres);
+
+
     
     
     const backToHome = () => {
@@ -12,6 +31,10 @@ function DetailsPage() {
     }
     return (
         <>
+        <h2>{currentMovie.title}</h2>
+        <img src={currentMovie.poster} alt={currentMovie.title}/>
+        <h1>{genres.join(', ')}</h1>
+        <p>{currentMovie.description}</p>
         <button onClick = {backToHome} >Back to Home</button>
         </>
     );
